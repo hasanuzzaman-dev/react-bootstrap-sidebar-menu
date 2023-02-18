cd8c7063bd281c4e0e11299a368a37b4ad6054bb
hasanuzzaman-dev <hasanuzzaman.dev@gmail.com>
2023-02-18 16:27:05 +0600

    add submenu

--- src/components/sidebar/MySidebar.js
+++ src/components/sidebar/MySidebar.js
@@ -1,6 +1,7 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from "react";
 import '../../css/MySidebarMenu.css'
+import 'bootstrap/js/dist/collapse'
 
 
 const MySidebar = () => {
@@ -15,9 +16,9 @@ const MySidebar = () => {
                         <i className="fs-4 bi bi-speedometer"></i>
                         <span className="ms-3 fs-4 d-none d-sm-inline">Brand</span>
                     </a>
-                    <hr className='text-secondary'/>
+                    <hr className='text-secondary d-none d-sm-block'/>
 
-                    <ul class="nav nav-tabs  flex-column">
+                    <ul class="nav nav-pills flex-column" id='parentMenu'>
                         <li class="nav-item text-white fs-4 my-1">
                             <a href="#" class="nav-link text-white fs-5" aria-current="page">
                                 <i className="fs-4 bi bi-speedometer2"></i>
@@ -31,11 +32,25 @@ const MySidebar = () => {
                             </a>
                         </li>
                         <li class="nav-item text-white fs-4 my-1">
-                            <a href="#" class="nav-link text-white fs-5" aria-current="page">
+                            <a href="#submenu" class="nav-link text-white fs-5" data-bs-toggle = "collapse" aria-current="page">
                                 <i className="fs-4 bi bi-grid"></i>
                                 <span className="ms-3 d-none d-sm-inline">Products</span>
+                                <span className="ms-2 bi bi-caret-down-fill"></span>
                             </a>
+                            <ul class="nav flex-column collapse ms-2" id='submenu' data-bs-parent='#parentMenu'>
+                                <li class="nav-item text-white">
+                                    <a class="nav-link text-white" href="#" aria-current="page">
+                                        Item 1
+                                    </a>
+                                </li>
+                                <li class="nav-item text-white">
+                                    <a class="nav-link text-white" href="#">Item 2</a>
+                                </li>
+                                
+                            </ul>
                         </li>
+
+
                         <li class="nav-item text-white fs-4 my-1">
                             <a href="#" class="nav-link text-white fs-5" aria-current="page">
                                 <i className="fs-4 bi bi-table"></i>
